6dfef2a9bd5e889f3fe7d2db2f4a37b978b3f232
Michael <mjh65@github.com>
2026-04-06 11:15:46 +0100

    Further updates for cmake v4 and gcc-15, as installed in latest MinGW.

--- extern/geotiff/patch.cmake
+++ extern/geotiff/patch.cmake
@@ -4,5 +4,6 @@
 file(READ "${AVITAB_geotiff_SOURCE_DIR}/libgeotiff/CMakeLists.txt" FILE_CONTENTS)
 
 string(REPLACE "(VERSION 2.6.0)" "(VERSION 3.23.0)" FILE_CONTENTS "${FILE_CONTENTS}")
+string(REPLACE "cmake_policy" "#cmake_policy" FILE_CONTENTS "${FILE_CONTENTS}")
 
 file(WRITE "${AVITAB_geotiff_SOURCE_DIR}/libgeotiff/CMakeLists.txt" "${FILE_CONTENTS}")
